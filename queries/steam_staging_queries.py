4452ab5895e6cd02361eefbf09ab52139072a574
Software Introspectre <softwareintrospectre@gmail.com>
2025-06-24 07:38:19 -0400

    Cleaning, adding inserts to downstream tables, and automating file cleanup

---
+++ queries/steam_staging_queries.py
@@ -0,0 +1,26 @@
+import mysql.connector
+import os
+
+def query_database():
+    db_config = {
+        'user': os.getenv('MYSQL_USER'),
+        'password': os.getenv('MYSQL_PASSWORD'),
+        'host': 'localhost',
+        'database': 'drm_free_games_db'
+    }
+
+    db_connection = mysql.connector.connect(**db_config)
+    cursor = db_connection.cursor()
+
+    # Example query to fetch all records
+    cursor.execute("SELECT * FROM steam_staging;")
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db_connection.close()
+
+if __name__ == '__main__':
+    query_database()
